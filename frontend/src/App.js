590b3b0b3d9747b3789a5b3045e89c61d0d2ec74
thuyavan <kannanthuyavan@gmail.com>
2023-05-09 17:51:23 -0400

    initial created app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> amazon clone </a>
+      </header>
+      <main>list of producs</main>
+    </div>
+  );
+}
+
+export default App;
